d486b10592913d7368ca4ba8a2102447255978d8
charlesoft <charlees1509@hotmail.com>
2014-05-17 18:25:47 -0300

    Implementado os metodos da classe ClienteController

---
+++ produto/código/Carefour/website/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+using WebSite.Persistence;
+
+namespace WebSite.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //
+        // GET: /Cliente/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmaCompra(Pedido pedido)
+        {
+            Cliente cliente = new Cliente();
+            return View(cliente);
+        }
+
+
+        public ActionResult CompraFinalizada(Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmaCompra(Cliente cliente)
+        {
+            ClienteDAO clienteDAO = new ClienteDAO();
+            clienteDAO.InserirCliente(cliente);
+
+            return RedirectToAction("CompraFinalizada",cliente);
+        }
+
+       
+
+    }
+}
---
+++ produto/código/Carefour/website/Persistence/ItemDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSite.Models;
+using MySql.Data.MySqlClient;
+
+namespace WebSite.Persistence
+{
+    public class ItemDAO : MySQL
+    {
+        public void InserirItems(List<Item> listaItems)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+
+            foreach (Item item in listaItems)
+            {
+                cmd.CommandText = "INSERT INTO tb_items (nome, descricao, preco, urlImagem, quantidade, id_peidido) VALUES ('" + item.nome + "','" + item.descricao + "'," + item.preco + ",'" + item.urlImagem + "'," + item.quantidade + "," + item.pedido.id + ")";
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
+        }
+    }
+}
