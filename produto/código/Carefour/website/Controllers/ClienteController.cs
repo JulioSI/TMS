1cd0b42aec04e4bda8cfb8139d7a8699f3b41196
Julio <si.juliocesar@gmail.com>
2014-05-18 02:23:07 -0300

    Atualização no ClienteController

--- produto/código/Carefour/website/Controllers/ClienteController.cs
+++ produto/código/Carefour/website/Controllers/ClienteController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSite.Models;
+using WebSite.Persistence;
 
 namespace WebSite.Controllers
 {
@@ -15,6 +17,29 @@ namespace WebSite.Controllers
         {
             return View();
         }
+
+        public ActionResult ConfirmaCompra(Pedido pedido)
+        {
+            Cliente cliente = new Cliente();
+            return View(cliente);
+        }
+
+
+        public ActionResult CompraFinalizada(Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmaCompra(Cliente cliente)
+        {
+            ClienteDAO clienteDAO = new ClienteDAO();
+            clienteDAO.InserirCliente(cliente);
+
+            return RedirectToAction("CompraFinalizada",cliente);
+        }
+
+       
 
     }
 }
